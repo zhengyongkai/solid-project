4c1fd2339633c3063b8bf9c6e71d9388a12b95b1
zhengyongkai <825947557@qq.com>
2024-02-20 20:53:57 +0800

    ✨ feat(css):
    1. 加入 tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,11 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = createSignal(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
+    <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p class="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
+      <p class="text-gray-500 text-lg">Hello React and tailwindcss</p>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { render } from 'solid-js/web';
 import { Router, Route } from '@solidjs/router';
 
+import './index.css';
 import Home from './App';
 
 render(
