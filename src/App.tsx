1038d461f9385756d235db2af39b2ad108db4b09
zhengyongkai <825947557@qq.com>
2024-02-23 21:26:42 +0800

    📃 docs: doc
    1. 更新内容

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { createSignal } from 'solid-js';
-import solidLogo from './assets/solid.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p class="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
-      <p class="text-gray-500 text-lg">Hello React and tailwindcss</p>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
