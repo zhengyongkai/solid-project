e57b161758ea96f5aabfd7392ecb64c85d8a6d1d
zhengyongkai <825947557@qq.com>
2024-05-07 23:01:33 +0800

    ✨ feat(page):
    1. 格式化

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,35 @@
-import { render } from "solid-js/web";
-import { HashRouter, Navigate, Route, Router } from "@solidjs/router";
+import { render } from 'solid-js/web';
+import { HashRouter, Navigate, Route } from '@solidjs/Router';
 
-import "./index.css";
-import { asyncRoutes, baseRoutes } from "@/router/index";
-import "cui-solid/dist/styles/cui.css";
+import './index.css';
+import { asyncRoutes, baseRoutes } from '@/router/index';
+import 'cui-solid/dist/styles/cui.css';
 
-import "@/assets/styles/index.scss";
-import "@/assets/styles/cui.scss";
-import "@/assets/styles/flow.scss";
-import "@/assets/styles/svg.scss";
+import '@/assets/styles/index.scss';
+import '@/assets/styles/cui.scss';
+import '@/assets/styles/flow.scss';
+import '@/assets/styles/svg.scss';
 
-import "virtual:svg-icons-register";
+import 'virtual:svg-icons-register';
 
-import { I18nProvider } from "solid-i18n";
-import i18n from "./locale";
-import useCommonStore from "@/stores/common/index";
-import { routeInf } from "./types";
-import { lazy } from "solid-js";
+import { I18nProvider } from 'solid-i18n';
+import i18n from './locale';
+import useCommonStore from '@/stores/common/index';
+import { routeInf } from './types';
+import { lazy } from 'solid-js';
 
-const Home = lazy(() => import("@/components/layout/BaseLayout"));
+const Home = lazy(() => import('@/components/layout/BaseLayout'));
 
 const [language] = useCommonStore().data.lang;
 
 const lang = i18n.getInstance(language());
 
-const renderRouter = (route: routeInf[]) => {
+const renderHashRouter = (route: routeInf[]) => {
   return route.map((i) => {
     if (i.children?.length) {
       return (
         <Route path={i.path} component={i.component}>
-          {renderRouter(i.children)}
+          {renderHashRouter(i.children)}
         </Route>
       );
     }
@@ -40,17 +40,17 @@ const renderRouter = (route: routeInf[]) => {
 render(
   () => (
     <I18nProvider i18n={lang}>
-      <Router>
-        {renderRouter(baseRoutes)}
-        <Route component={Home} path={"/"}>
+      <HashRouter>
+        {renderHashRouter(baseRoutes)}
+        <Route component={Home} path={'/'}>
           <Route
-            path={"*"}
-            component={() => <Navigate href={"/home"}></Navigate>}
+            path={'*'}
+            component={() => <Navigate href={'/home'}></Navigate>}
           ></Route>
-          {renderRouter(asyncRoutes)}
+          {renderHashRouter(asyncRoutes)}
         </Route>
-      </Router>
+      </HashRouter>
     </I18nProvider>
   ),
-  document.getElementById("root")!
+  document.getElementById('root')!
 );
